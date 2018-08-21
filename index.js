9ee6976dd12e9d28430e43edd72041a476cc6c71
Matt Mitchell <mattmitchell650@gmail.com>
2018-08-21 16:14:46 -0700

    cleaned up

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const JWT = require('jsonwebtoken');
 const axios = require('axios');
 const uuid = require('uuid')
 
-const clientId = "ostkg0vclqzee2j5l5ketncrctah2nn0"
-const clientSecret = "psqn9Ih7I1HcbNwTsnDphKHAemw6mHSn"
-
 const uri = "https://api.box.com/oauth2/token"
 const jwtGrantType = "urn:ietf:params:oauth:grant-type:jwt-bearer"
 const options = {
-  "iss": clientId,
-  "sub": "3924970579", // unique app user id
-  "box_sub_type": "user",
+  "iss": BoxConfig.clientID,
+  "sub": BoxConfig.enterpriseID,
+  "box_sub_type": "enterprise",
   "aud": "https://api.box.com/oauth2/token",
   "jti": uuid.v4(), // unique string
   "exp": Math.floor(Date.now() / 1000) + 20 // create expiry for 20s in the future
@@ -24,12 +21,13 @@ const assertion = JWT.sign(
   { algorithm: "RS256" }
 );
 
-console.log(assertion);
-
 // format post body
-const body = `grant_type=${jwtGrantType}&client_id=${clientId}&client_secret=${clientSecret}&assertion=${assertion}`
+const body = `grant_type=${jwtGrantType}` +
+  `&client_id=${BoxConfig.clientID}` +
+  `&client_secret=${BoxConfig.clientSecret}` +
+  `&assertion=${assertion}`
 
-// send request to fetch user access token
+// send request to fetch service account's enterprise access token
 axios.post(uri, body)
   .then(function (response) {
     console.log(response.data);
